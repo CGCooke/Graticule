516c66ec17385656cb86df27c4db8a839ea743cb
Cameron Cooke <Cameron.G.Cooke@Gmail.com>
2021-04-07 22:24:51 +0200

    Adding in tag detection code

--- Scene.py
+++ Scene.py
@@ -31,52 +31,112 @@ The position, orientation, and ID of a tag, as observed by the camera.
 import glob
 import numpy as np
 import cv2
-import cv2.aruco as aruco
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-
 from scipy.spatial.transform import Rotation as Rot
-from scipy.optimize import least_squares
-from scipy.sparse import lil_matrix
-
-
-class Scene(object):
-	"""docstring for Scene"""
-	def __init__(self, img_dir, aruco_tag_type):
-		self.img_dir = img_dir
-		self.aruco_tag_type = aruco_tag_type
-		self.Tags = []
-		self.Observations = []
-
-
-class Tag(object):
-	"""docstring for Tag"""
-	def __init__(self):
-		pass
-
-class Observation(object):
-	"""docstring for Observation"""
-	def __init__(self, img_path):
-		self.img_path = []
-		self.Camera = Camera()
-		self.TagObservations = []
-
-	def extract_tags(self):
-		pass
-		
-class Camera(object):
-	"""docstring for Camera"""
-	def __init__(self):
-		pass
-
-class TagObservation(object):
-	"""docstring for TagObservation"""
-	def __init__(self):
-		pass
 
 class Pose(object):
 	"""docstring for Pose"""
 	def __init__(self):
-		pass
+		self.rotation = None
+		self.t = None
+		self.coordinate_system = None
+
+class Scene(object):
+	"""docstring for Scene"""
+	def __init__(self, img_dir):
+		self.img_dir = img_dir
+		self.Tags = []
+		self.Observations = []
+
+class Tag(object):
+	"""docstring for Tag"""
+	def __init__(self):
+		self.estimated_pose = None
+		self.tag_id = None
+
+class Observation(object):
+	"""docstring for Observation"""
+	def __init__(self, img_path, camera_id):
+		self.img_path = img_path
+		self.camera_id = camera_id
+		self.Camera = Camera(camera_id)
+		self.TagObservations = []
+
+	def extract_tags(self):
+		marker_size = 1.2 #units
+		points_3d = np.array([[0.0,marker_size,0.0],
+		                      [marker_size,marker_size,0.0],
+		                      [marker_size,0.0,0.0],
+		                      [0.0,0.0,0.0]])
+
+		points_3d[:,0:2] -= marker_size/2.0
+
+
+		arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+		arucoParams = cv2.aruco.DetectorParameters_create()
+		img = cv2.imread(self.img_path)
+		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # change img to grayscale
+		(corners, ids, rejected) = cv2.aruco.detectMarkers(gray, arucoDict, parameters = arucoParams)
+		
+		for tag_corners, tag_id in zip(corners, ids):
+			points_2d = tag_corners.squeeze()
+			R, t = self.find_tag_pose(points_3d, points_2d)
+
+			tag_observation = TagObservation(f'Tag_{int(tag_id)}')
+			tag_observation.rotation = R
+			tag_observation.t = t
+			tag_observation.coordinate_system = f'Camera_{self.camera_id}'
+			self.TagObservations.append(tag_observation)
+
+
+	def find_tag_pose(self, points_3d, points_2d):
+		K = self.Camera.K
+		dist = self.Camera.dist
+
+		ret, rvec, tvec = cv2.solvePnP(objectPoints = points_3d, imagePoints = points_2d, cameraMatrix = K, distCoeffs = dist, flags = cv2.SOLVEPNP_IPPE_SQUARE)
+
+		#Find tag R & t wrt camera.
+		R = Rot.from_rotvec(rvec[:,0])
+		t = np.array(tvec)
+		return(R,t)
+		
+		#Find Camera C & R wrt tag.
+		#C = -np.dot(R.T,tvec)
+		#R = Rot.from_matrix(R.T).as_matrix()
+
+class CameraIntrinsics(object):
+	"""docstring for CameraIntrinsics"""
+	def __init__(self):
+		focal_length_mm = 50 #mm
+		width_pixels = 800.0
+		height_pixels = 600.0
+		width_mm = 36.0
+		pixels_per_mm = width_pixels/width_mm
+		focal_length_pixels = pixels_per_mm * focal_length_mm
+
+		self.K = np.array([[focal_length_pixels, 0.0 , width_pixels/2],
+					  [0.0   , focal_length_pixels, height_pixels/2],
+					  [0.0   , 0.0   ,    1.0]])
+
+		self.dist = np.array([[0],[0],[0],[0],[0]])
+
+
+
+class Camera(Pose, CameraIntrinsics):
+	"""docstring for Camera"""
+	def __init__(self, camera_id):
+		Pose.__init__(self)
+		CameraIntrinsics.__init__(self)
+		self.camera_id = camera_id
+
+class TagObservation(Pose):
+	"""docstring for TagObservation"""
+	def __init__(self, tag_id):
+		Pose.__init__(self)
+		self.tag_id = tag_id
+
+
+
+		
+
 		
 		
--- test_Scene.py
+++ test_Scene.py
@@ -1,5 +1,18 @@
-from Scene import Scene
+from Scene import Scene, Observation, Camera, TagObservation
 import cv2
+import numpy as np
 
 def test_Scene():
-	scene = Scene('Test_data/*.jpg', aruco_tag_type = cv2.aruco.DICT_4X4_50)
+	scene = Scene('Test_data/*.jpg')
+	assert scene.img_dir = 'Test_data/*.jpg'
+
+def test_Observation():
+	observation = Observation('Test_data/0.jpg', camera_id = 0)
+	observation.extract_tags()
+
+	for observation in observation.TagObservations:
+		if observation.tag_id == 'Tag_0':
+			assert observation.coordinate_system == 'Camera_0'			
+			assert np.allclose(observation.t, np.array([[-3.3121481], [-3.51199432], [20.23151067]]))
+			assert np.allclose(observation.rotation.as_rotvec(), np.array([1.69683488, 1.6904307, -0.79607287]))
+			assert observation.tag_id == 'Tag_0'
